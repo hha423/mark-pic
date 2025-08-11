a1e2f857c0cdf70677bfc097cc23d954dd6ad94c
Alterem <alterem520@gmail.com>
2025-08-11 14:48:31 +0800

    feat(docs): 添加图片并更新链接

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ sequenceDiagram
 
 如果这个项目对你有帮助，欢迎请我喝杯咖啡 ☕
 
-<img src="https://github.com/alterem/mark-pic/raw/main/docs/reward-code.jpg" alt="赞赏码" width="300" />
+<img src="/mark-pic/reward-code.jpg" alt="赞赏码" width="300" />
 
 *"一杯咖啡，一声鼓励。"*
 `)
